1d485e1766bb3f269698b877b3eca0dddb42597f
webwww74 <115360573+webwww74@users.noreply.github.com>
2024-04-02 22:10:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git-full packages https://git.openwrt.org/feed/packages.git;openwrt-23.05 >>feeds.conf.default
-echo 'src-git-full luci https://git.openwrt.org/project/luci.git;openwrt-23.05 >>feeds.conf.default
-echo 'src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-23.05 >>feeds.conf.default
-echo 'src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-23.05 >>feeds.conf.default
+echo 'src-git-full packages https://git.openwrt.org/feed/packages.git;openwrt-23.05' >>feeds.conf.default
+echo 'src-git-full luci https://git.openwrt.org/project/luci.git;openwrt-23.05' >>feeds.conf.default
+echo 'src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-23.05' >>feeds.conf.default
+echo 'src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-23.05' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
